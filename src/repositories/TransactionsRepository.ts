9fcb69a3e7303722fb12c5ccd96b13966ed1718f
Jin <oliveira.jinbok@gmail.com>
2020-06-22 21:53:09 -0300

    Chanllanged completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,53 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome'
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
   constructor() {
     this.transactions = [];
+
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((total, transaction) => {
+      if(transaction.type === 'income') return total + transaction.value
+
+      return total;
+    }, 0);
+
+    const outcome = this.transactions.reduce((total, transaction) => {
+      if(transaction.type === 'outcome') return total + transaction.value
+
+      return total;
+    }, 0);
+
+    const balance = {
+      income, 
+      outcome,
+      total: income - outcome,
+    }
+
+    return balance;
+
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
